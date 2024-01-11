e20723da9b1daf673d327db4f34d2fd314cb117a
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-11 11:17:15 -0500

    trying to make it so a readme file is created

--- index.js
+++ index.js
@@ -7,67 +7,75 @@ const questions = [{
     type: 'input',
     name: 'title',
     message: 'whats the title?',
-},{
+}, {
     type: 'input',
     name: 'description',
-    message:'whats your description?'
-},{
+    message: 'whats your description?'
+}, {
     type: 'input',
     name: 'table_of_contents',
-    message:'what are your table of contents?'
+    message: 'what are your table of contents?'
 
-},{
+}, {
     type: 'input',
     name: 'usage',
-    message:'what is the usage information?'
-},{
+    message: 'what is the usage information?'
+}, {
     type: 'list',
     name: 'license',
-    message:'please choose a license?',
+    message: 'please choose a license?',
     choices: ['MIT']
-},{
+}, {
     type: 'input',
     name: 'contributing',
-    message:'who contributed?'
-},{
+    message: 'who contributed?'
+}, {
     type: 'input',
     name: 'tests',
-    message:'are there any tests?'
-},{
+    message: 'are there any tests?'
+}, {
     type: 'input',
     name: 'questions',
-    message:'are there any questions?'
+    message: 'are there any questions?'
 }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    generateMarkdown(data);
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log('Error making README.md file', err);
+        } else {
+            console.log('README.md file created successfully.')
+        }
+    })
+    // generateMarkdown(data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
- inquirer
-  .prompt(questions)
-  .then((answers) => {
-    const makeReadMe = generateMarkdown(answers);
-    fs.writeFile('README.md', makeReadMe, (err) => {
-        if(err){
-            console.log('Error making README.md file', err);
-        } else{
-            console.log('README.md file created successfully.')
-        }
-    })
-    // const badgeImageURL = `![License]((https://img.shields.io/badge/License-${answers.license}-blue.svg))`
-    // console.log(badgeImageURL)
-    // console.log(answers);
-    //writeToFile('README.md', data) //data equals to the user inputs
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-    } else {
-    }
-  });
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const makeReadMe = generateMarkdown(answers);
+            // fs.writeFile('./README.md', makeReadMe, (err) => {
+            //     if(err){
+            //         console.log('Error making README.md file', err);
+            //     } else{
+            //         console.log('README.md file created successfully.')
+            //     }
+            // })
+            // const badgeImageURL = `![License]((https://img.shields.io/badge/License-${answers.license}-blue.svg))`
+            // console.log(badgeImageURL)
+            // console.log(answers);
+            //writeToFile('README.md', data) //data equals to the user inputs
+            writeToFile('./README.md', makeReadMe);
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            } else {
+            }
+        });
 }
 
 // Function call to initialize app
